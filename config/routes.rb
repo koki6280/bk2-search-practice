84e14878e2852eda0d0f063e1ecb4aba63807a51
vagrant <vagrant@localhost.localdomain>
2020-07-10 06:25:07 +0000

    search-practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  
+
   resources :books do
   	resources :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
-  
+
   root 'home#top'
   get 'home/about'
+  get '/search', to: 'search#search'
 end
